e6280d439fd7dd9e85d2b82130cd2ba4482dd6e1
abraham <abrahamtoledo90@gmail.com>
2019-06-23 18:59:12 -0400

    Second Commit

--- www/index.php
+++ www/index.php
@@ -1,14 +1,33 @@
 <?php
-$scheme = $_SERVER['HTTPS'] ? "https" : "http";
-$url = "$scheme://go.auroraml.com" . "/";
-header("Location: ".  $url);
-?>
 
-<html>
-<head>
-	<title>Redirection</title>
-</head>
-<body>
-	<p>The document has moved <a href="<?php print $url; ?>">here</a></p>
-</body>
-</html>
+define("__ROOT__", __DIR__);
+
+require "libs/smarty/Smarty.class.php";
+
+$smarty = new Smarty;
+
+$smarty->clearAllCache();
+
+$smarty->addTemplateDir(__ROOT__ . "/views");
+$smarty->setCaching(false);
+
+$template = !empty($_GET['page']) ? $_GET['page'] : "home";
+$template .= ".tpl";
+
+if ($smarty->templateExists($template)){
+
+    if ($template == "home.tpl"){
+        $apps = json_decode(file_get_contents("downloads/apps_manifest.json"), true);
+        //var_dump($apps);
+        $smarty->assign("apps", $apps['apps']);
+    }
+
+    $smarty->display($template);
+    exit;
+}else{
+    header("HTTP/1.1 404 Page Not Found");
+    $smarty->display("error_404.tpl");
+    exit;
+}
+
+
