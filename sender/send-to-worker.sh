db66c1e14c783660b288567b58b4ccbe7152730f
abraham <abrahamtoledo90@gmail.com>
2019-06-24 23:52:45 -0400

    Added Log capabilities to send-to-worker.sh

--- sender/send-to-worker.sh
+++ sender/send-to-worker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+logfile="/var/log/send-to-woerker"
+
+logger -f "${logfile}" "Request received from $1"
+
 . /etc/auroraml/conf.sh
 
 # Save mail message to a temporary file
@@ -8,6 +12,7 @@ cat > ${tmpfile}
 
 # Send request to workers
 curl -F "email=@${tmpfile}" http://${WORKERS_BALANCER}/run.php -s > null &
+logger -f "${logfile}" "Request sent to workers pool"
 
 # Pause for 100 miliseconds then safely delete temp file
 sleep 0.1
